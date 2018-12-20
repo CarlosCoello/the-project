22a3e2f6b0d6154b6de6b3847a8ae3df1ddf64e1
Carlos Coello <carlos.coello05@outlook.com>
2018-12-20 02:58:12 -0600

    added components, routingmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// our components so the app can recognize them and utilize them
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+
+// AppRoutingModule for our routes to work across the app
+import { AppRoutingModule } from './app.routing';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    MainNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
