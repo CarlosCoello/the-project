20e7e744e2570d94ff6a49a459145d900f58b1d8
Carlos Coello <carlos.coello05@outlook.com>
2019-01-04 01:30:33 -0600

    added controller

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const PORT = process.env.PORT || 4000;
-const path = require('path');
-const mongoose = require('mongoose');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const PORT = process.env.PORT || 4000
+const path = require('path')
+const mongoose = require('mongoose')
 
 // connect to db
 mongoose.connect(
   'mongodb://patitas:Chatita2018@ds141294.mlab.com:41294/heroku_p9m6t1b5',
   { useNewUrlParser: true }
-);
+)
 
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-app.use(express.static(__dirname + '/dist/the-project'));
-
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/the-project-patitas/index.html'));
-});
+app.use(express.static(__dirname + '/dist/the-project'))
 
 // importing controllers
-const UserController = require('./Controllers/UserController');
-app.use('/user', UserController);
+const UserController = require('./Controllers/UserController')
+app.use('/user', UserController)
+const ProductController = require('./Controllers/ProductController')
+app.use('/product', ProductController)
 
-app.listen(PORT, () => console.log(`listening no port ${PORT}`));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/the-project-patitas/index.html'))
+})
+
+app.listen(PORT, () => console.log(`listening no port ${PORT}`))
