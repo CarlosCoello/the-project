c93a5d1d64a2b07bac198b7e98cd62735a2f5669
Carlos Coello <carlos.coello05@outlook.com>
2019-01-04 01:28:26 -0600

    product controller

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,36 +1,42 @@
-const User = require('../models/User');
-const express = require('express');
-const router = express.Router();
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const User = require('../models/User')
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 router.post('/registerMe', (req, res) => {
-    console.log(req.body);
-    if(req.body) {
-        if(req.body.name) {
-            if(req.body.email) {
-                if(req.body.password) {
-                    const hash = bcrypt.hashSync(req.body.password, saltRounds);    User.create({
-                            name: req.body.name,
-                            email: req.body.email,
-                            password: hash
-                        }, function(err, user) {
-                            if(err) {
-                                res.status(400).send({ success: false, message: err });
-                            } else {
-                                res.status(200).send({ success: true, message: user });
-                            }
-                        });
-                } else {
-                    res.status(400).send({ success: false, message: 'password is required' })
-                }
-            } else {
-                res.status(400).send({ success: false, message: 'email is required' });
+  console.log(req.body)
+  if (req.body) {
+    if (req.body.name) {
+      if (req.body.email) {
+        if (req.body.password) {
+          const hash = bcrypt.hashSync(req.body.password, saltRounds)
+          User.create(
+            {
+              name: req.body.name,
+              email: req.body.email,
+              password: hash
+            },
+            function(err, user) {
+              if (err) {
+                res.status(400).send({ success: false, message: err })
+              } else {
+                res.status(200).send({ success: true, message: user })
+              }
             }
+          )
         } else {
-            res.status(400).send({ success: false, message: 'name is required' });
+          res
+            .status(400)
+            .send({ success: false, message: 'password is required' })
         }
+      } else {
+        res.status(400).send({ success: false, message: 'email is required' })
+      }
+    } else {
+      res.status(400).send({ success: false, message: 'name is required' })
     }
-});
+  }
+})
 
-module.exports = router;
+module.exports = router
